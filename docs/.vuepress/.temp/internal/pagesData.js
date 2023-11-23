8fe7b7da8e5cba77c92f87680025c409980eff2b
lijing <lijing@aipark.com>
2023-11-23 11:16:56 +0800

    updateVuepress

---
+++ docs/.vuepress/.temp/internal/pagesData.js
@@ -0,0 +1,6 @@
+export const pagesData = {
+  // path: /404.html
+  "v-3706649a": () => import(/* webpackChunkName: "v-3706649a" */"D:/XiangMu/归档/github/vuePress/docs/.vuepress/.temp/pages/404.html.js").then(({ data }) => data),
+  // path: /
+  "v-8daa1a0e": () => import(/* webpackChunkName: "v-8daa1a0e" */"D:/XiangMu/归档/github/vuePress/docs/.vuepress/.temp/pages/index.html.js").then(({ data }) => data),
+}
---
+++ docs/README.md
@@ -0,0 +1,8 @@
+<!--
+ * @Author: lijing
+ * @Date: 2023-11-23 10:41:29
+ * @LastEditors: lijing
+ * @LastEditTime: 2023-11-23 10:41:37
+ * @Description: 
+-->
+# hello vuepress
